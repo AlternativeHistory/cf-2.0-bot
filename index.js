2e3c618838539f6ca4afc4242d90bd6b61135b12
AlternativeHistory <99097604+AlternativeHistory@users.noreply.github.com>
2025-01-03 16:03:24 -0600

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ client.on('ready', async () => { // When the client is "ready"
     console.log(`${client.user.username} has been successfully launched`)
 }); 
  
-const token = ("MTMyNDgzNDc2NDI3MTc4Mzk5Nw.GWYLN8.RKe--gBPD3elAV_9JH57P3D5IpgDzn-dd_KBvk") // Define your bot token here
+const token = ("MTMyNDgzNDc2NDI3MTc4Mzk5Nw.GEqMpM.WYwDPPSaIQA-n1MDLQpTUgWiJk06B0dQZacvCU") // Define your bot token here
  
 // The code for banning members 
  
